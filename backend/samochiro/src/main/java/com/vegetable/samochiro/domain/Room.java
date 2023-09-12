abfebb562c5b2c2534ee3b022a50dc3f51078a46
leunseongl <anytime0916@naver.com>
2023-09-12 17:57:22 +0900

    init

---
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/domain/Room.java
@@ -0,0 +1,36 @@
+package com.vegetable.samochiro.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "room")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room {
+
+	@Id @Column(name = "room_uuid", length = 36)
+	private String roomUuid;
+
+	@Column(name = "target_name" , length = 20)
+	private String targetName;
+
+	private int sequence;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+}
+
--- backend/samochiro/src/main/java/com/vegetable/samochiro/domain/User.java
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/domain/User.java
@@ -3,7 +3,10 @@ package com.vegetable.samochiro.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,16 +22,22 @@ import lombok.Setter;
 public class User {
 
 	@Id
-	@Column(name = "user_id")
+	@Column(name = "user_id", length = 50)
 	private String userId;
 
+	@Column(name = "password", length = 320)
 	private String email;
+
+	@Column(name = "nickname", length = 20)
 	private String nickname;
 
 	@Column(name = "is_change")
 	private boolean isChange;
 
-	@Column(name = "social_type")
+	@Column(name = "social_type", length = 20)
 	private String socialType;
+
+	@OneToMany(mappedBy = "user")
+	private List<Room> rooms = new ArrayList<>();
 
 }
