1630891589f7cddfc1db3d3b19f129d1cddf873a
Jiyoung Park <parkninni@icloud.com>
2023-09-12 15:15:34 +0900

    chore: #1 - 절대경로 추가, port번호 3000번으로 변경

--- frontend/memorial/vite.config.ts
+++ frontend/memorial/vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  assetsInclude: ["**/*.glb", "**/*.gltf", "**/*.obj"],
+  server: {
+    port: 3000,
+    host: true,
+    open: "/",
+  },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        assetFileNames: (asset) => {
+          const isCssFile = /\.css$/.test(asset.name);
+
+          if (isCssFile) {
+            return "static/css/[name].[hash].[ext]";
+          }
+          return "static/media/[name].[hash].[ext]";
+        },
+        chunkFileNames: "static/js/[name].[hash].chunk.js",
+        entryFileNames: "static/js/[name].[hash].js",
+      },
+    },
+    commonjsOptions: {
+      include: [".yarn/**"],
+    },
+  },
   cacheDir: ".yarn",
+  define: {
+    global: "globalThis",
+  },
 });
