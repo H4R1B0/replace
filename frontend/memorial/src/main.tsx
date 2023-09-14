7b106fcd65b0e6bd7ed7c8bf8ea6f4506409fe75
Jiyoung Park <parkninni@icloud.com>
2023-09-14 17:45:48 +0900

    feat: msw handler 및 browser 설정 완료

--- frontend/memorial/src/main.tsx
+++ frontend/memorial/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import initMocks from "./mocks/index.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+if (import.meta.env.VITE_APP_ENV === "development") {
+  await initMocks();
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ frontend/memorial/src/mocks/browser.ts
@@ -0,0 +1,5 @@
+// src/mocks/browser.ts
+import { setupWorker } from "msw";
+import { handlers } from "./handlers/handlers";
+
+export const worker = setupWorker(...handlers);
