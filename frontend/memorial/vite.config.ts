43db0b490d70e28e38413a315c72496184cb499f
Jiyoung Park <parkninni@icloud.com>
2023-09-14 10:29:15 +0900

    test: vite-config

--- frontend/memorial/vite.config.ts
+++ frontend/memorial/vite.config.ts
@@ -8,20 +8,5 @@ export default defineConfig({
     port: 3000,
     host: "0.0.0.0",
   },
-  build: {
-    rollupOptions: {
-      input: "src/main.tsx", // 이 부분을 프로젝트의 진입점 파일로 설정
-    },
-    commonjsOptions: {
-      include: [".yarn/**"],
-    },
-  },
   cacheDir: "./.vite",
-  define: {
-    global: "globalThis",
-  },
-  //
-  optimizeDeps: {
-    include: ["*.tsx"], // 모든 .js 파일을 포함하도록 설정
-  },
 });
