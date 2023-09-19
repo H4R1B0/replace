fa34e0f80ed31efa5853ca0607bbe1a2dc5ae75a
Jiyoung Park <parkninni@icloud.com>
2023-09-19 16:46:16 +0900

    feat: #16 - 방 시야각 조정 및 방 시야 조절시 발생하는 touchAction 에러 해결

--- frontend/memorial/src/pages/RoomPage/index.tsx
+++ frontend/memorial/src/pages/RoomPage/index.tsx
@@ -1,16 +1,35 @@
 import Room from "@components/3d/Room";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stage } from "@react-three/drei";
+import {
+  OrbitControls,
+  Stage,
+  Edges,
+  PresentationControls,
+} from "@react-three/drei";
 import styles from "./RoomPage.module.css";
 
 export default function RoomPage() {
   return (
     <div className={styles.wrapper}>
-      <Canvas>
-        <OrbitControls />
+      <Canvas
+        flat
+        dpr={[1, 2]}
+        camera={{ fov: 50, position: [0, 0, 8] }}
+        style={{ touchAction: "none" }}
+      >
+        <color attach="background" args={["#e0b7ff"]} />
         <Stage environment="city" intensity={0.5} adjustCamera>
-          {/* <ambientLight /> */}
-          <Room onTrashcanClick={() => console.log("nini")} />
+          <PresentationControls
+            snap
+            global
+            zoom={1.5}
+            rotation={[0, -Math.PI / 4, 0]}
+            polar={[0, Math.PI / 4]}
+            azimuth={[-Math.PI / 4, Math.PI / 4]}
+          >
+            {/* <ambientLight /> */}
+            <Room onTrashcanClick={() => console.log("nini")} />
+          </PresentationControls>
         </Stage>
       </Canvas>
     </div>
