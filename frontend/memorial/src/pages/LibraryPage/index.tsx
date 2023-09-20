77095e328ab81d87a07fe2ae3523c5376361655e
JiwonShin <acd0825@naver.com>
2023-09-20 09:23:19 +0900

    Add: #9 - 오브젝트 클릭 시 모달 창 팝업 구현

--- frontend/memorial/src/pages/LibraryPage/index.tsx
+++ frontend/memorial/src/pages/LibraryPage/index.tsx
@@ -2,8 +2,12 @@ import Library from "@components/3d/Library";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import styles from "./LibraryPage.module.css";
+import Modal from "@components/ui/Modal";
+import { useState } from "react";
 
 export default function LibraryPage() {
+  const [letterModalOpen, setLetterModalOpen] = useState(false);
+  const [bookModalOpen, setBookModalOpen] = useState(false);
   return (
     <div className={styles.wrapper}>
       <Canvas>
@@ -11,11 +15,27 @@ export default function LibraryPage() {
         <Stage environment="city" intensity={0.5} adjustCamera>
           {/* <ambientLight /> */}
           <Library
-            onLetterClick={() => console.log("편지")}
-            onBookShelfClick={() => console.log("책장")}
+            onLetterClick={() => setLetterModalOpen(true)}
+            onBookShelfClick={() => setBookModalOpen(true)}
           />
         </Stage>
       </Canvas>
+      <Modal
+        modalOpen={letterModalOpen}
+        onClose={() => setLetterModalOpen(false)}
+        title="모달을 테스트 해봐요"
+        buttonLabel="확인"
+      >
+        모달안에서 이동하는 방법 고민해보기
+      </Modal>
+      <Modal
+        modalOpen={bookModalOpen}
+        onClose={() => setBookModalOpen(false)}
+        title=" 두 번째 모달도 테스트 해봐요"
+        buttonLabel="확인"
+      >
+        링크 변경이 아니잖아
+      </Modal>
     </div>
   );
 }
