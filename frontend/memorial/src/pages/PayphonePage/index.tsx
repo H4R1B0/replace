57cd86d6433ddb84ddbe905d0390fea852f1ee20
Juahjoah <07juah21@gmail.com>
2023-09-20 14:14:49 +0900

    feat: #17 - 공중전화 시야각 조절 및 구현 확인

--- frontend/memorial/src/pages/PayphonePage/index.tsx
+++ frontend/memorial/src/pages/PayphonePage/index.tsx
@@ -1,23 +1,47 @@
 import Payphone from "@components/3d/Payphone";
+
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stage, PresentationControls } from "@react-three/drei";
+import { Stage, PresentationControls } from "@react-three/drei";
 import styles from "./PayphonePage.module.css";
+import {
+  Selection,
+  EffectComposer,
+  Outline,
+} from "@react-three/postprocessing";
 
 export default function PayphonePage() {
   return (
     <div className={styles.wrapper}>
-      <Canvas flat camera={{ fov: 25, position: [0, 0, 8] }}>
-        <OrbitControls />
-        <Stage environment="city" intensity={0.5} adjustCamera>
+      <Canvas
+        flat
+        dpr={[1, 2]}
+        camera={{ fov: 25, position: [0, 0, 8] }}
+        style={{ touchAction: "none" }}
+      >
+        {/* <OrbitControls /> */}
+        <Stage environment="city" intensity={0.5} adjustCamera shadows={false}>
           <PresentationControls
             snap
             global
-            zoom={0.8}
+            zoom={2.0}
             rotation={[0, -Math.PI / 2.5, 0]}
             polar={[0, Math.PI / 4]}
             azimuth={[-Math.PI / 4, Math.PI / 4]}
           >
-            <Payphone onPayphoneClick={() => console.log("여기가 공중전화?")} />
+            <Selection>
+              <EffectComposer multisampling={8} autoClear={false}>
+                <Outline
+                  blur
+                  visibleEdgeColor={0xffffff}
+                  edgeStrength={100}
+                  width={1000}
+                />
+              </EffectComposer>
+              <Payphone
+                onPayphoneClick={() => console.log("여기가 공중전화?")}
+              />
+              {/* <Payphone onPayphoneClick={openModal} /> */}
+            </Selection>
           </PresentationControls>
         </Stage>
       </Canvas>
