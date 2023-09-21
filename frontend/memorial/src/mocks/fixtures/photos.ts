6c0fa2d6a98fb0d684a047c1dbf99e41473ada0a
Jiyoung Park <parkninni@icloud.com>
2023-09-21 17:51:02 +0900

    feat: 방에 관련된 모든 api 작성 완료 및 api 로직 분리

---
+++ frontend/memorial/src/mocks/fixtures/photos.ts
@@ -0,0 +1,24 @@
+import type { PhotoList } from "../../types/Photo";
+
+const photos: PhotoList = {
+  photos: [
+    {
+      photoId: 1,
+      photoUrl: "http://example.com/photo1.jpg",
+      registDate: "2023-09-05 12:50:23",
+    },
+    {
+      photoId: 2,
+      photoUrl: "http://example.com/photo2.jpg",
+      registDate: "2023-09-05 12:50:23",
+    },
+    {
+      photoId: 3,
+      photoUrl: "http://example.com/photo3.jpg",
+      registDate: "2023-09-05 12:50:23",
+    },
+  ],
+  total: 3,
+};
+
+export default photos;
