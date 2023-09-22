0e072d17d03fe9560a5299be32d3c2c193685df8
Jiyoung Park <parkninni@icloud.com>
2023-09-22 16:32:00 +0900

    chore: devtools 삭제

--- frontend/memorial/src/main.tsx
+++ frontend/memorial/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ if (import.meta.env.DEV) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
