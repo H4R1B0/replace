4e177e4ee1284bffba998043754fadabf1fe984f
JiwonShin <acd0825@naver.com>
2023-09-22 17:28:19 +0900

    add: 헌화 생성 페이지 인풋 태그 추가

--- frontend/memorial/src/pages/CreateTributePage/index.tsx
+++ frontend/memorial/src/pages/CreateTributePage/index.tsx
@@ -1,3 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import PATH from "@constants/path";
 export default function CreateTributePage() {
-  return <div>헌화 생성</div>;
+  const navigate = useNavigate();
+  const days = [];
+  for (let i = 1; i <= 30; i++) {
+    days.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+  }
+  return (
+    <div>
+      <h3>소중한 사람을 위한 헌화 공간</h3>
+
+      <p>추모 제목</p>
+      <input type="text" />
+
+      <p>추모 기간</p>
+      <select name="tributePeriod" id="tributePeriod">
+        {days}
+      </select>
+
+      <p>추모 제목</p>
+      <input type="text" />
+
+      <p>추모 문구</p>
+      <input type="text" />
+
+      <p>추모 인물에 대한 설명을 작성해주세요. </p>
+      <input type="text" />
+
+      <button>작성 하기</button>
+      <button onClick={() => navigate(`${PATH.TRIBUTELIST}`)}>목록 보기</button>
+    </div>
+  );
 }
