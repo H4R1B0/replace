2831eb327a0f5372b57940a29562a6bbdc76ccd6
leunseongl <anytime0916@naver.com>
2023-09-23 16:46:59 +0900

    "feat: 헌화 등록 시 알고리즘 기능 추가"

---
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/exception/NegativeWordException.java
@@ -0,0 +1,9 @@
+package com.vegetable.samochiro.exception;
+
+public class NegativeWordException extends RuntimeException {
+
+	public NegativeWordException(String message) {
+		super(message);
+	}
+
+}
