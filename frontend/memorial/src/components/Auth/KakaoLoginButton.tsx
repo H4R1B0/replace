bdfe128eb666cd104a783f5a7a5c9d25eddc852e
Juahjoah <07juah21@gmail.com>
2023-09-25 02:48:27 +0900

    feat: #31 - 카카오 소셜 로그인 버튼-화면 연결

---
+++ frontend/memorial/src/components/Auth/KakaoLoginButton.tsx
@@ -0,0 +1,16 @@
+export default function KakaoLoginButton() {
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleKakaoLogin = () => {
+    window.location.href = KAKAO_URL;
+  };
+
+  return (
+    <div>
+      <button onClick={handleKakaoLogin}>카카오 로그인</button>
+    </div>
+  );
+}
