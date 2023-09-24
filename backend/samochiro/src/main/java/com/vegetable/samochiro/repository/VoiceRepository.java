315ae000db2cb7a78e9039a66deb617ca52add44
박현준 <qkrguswns25@naver.com>
2023-09-24 18:27:25 +0900

    Feat be radio

--- backend/samochiro/src/main/java/com/vegetable/samochiro/repository/VoiceRepository.java
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/repository/VoiceRepository.java
@@ -2,8 +2,13 @@ package com.vegetable.samochiro.repository;
 
 import com.vegetable.samochiro.domain.Voice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface VoiceRepository extends JpaRepository<Voice, String> {
+public interface VoiceRepository extends JpaRepository<Voice, Long> {
+    @Query("select v from Voice v where v.room.uuid=:roomUuid")
+    List<Voice> findByRoomUuid(String roomUuid);
 }
---
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/service/RadioService.java
@@ -0,0 +1,57 @@
+package com.vegetable.samochiro.service;
+
+import com.vegetable.samochiro.domain.Voice;
+import com.vegetable.samochiro.dto.radio.RadioVoiceResponse;
+import com.vegetable.samochiro.dto.radio.RadioVoicesResponse;
+import com.vegetable.samochiro.dto.radio.VoiceItem;
+import com.vegetable.samochiro.repository.RoomRepository;
+import com.vegetable.samochiro.repository.VoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RadioService {
+
+    private final RoomRepository roomRepository;
+    private final VoiceRepository voiceRepository;
+
+    public RadioVoicesResponse getVoices(String userId, int sequence) {
+        String roomUuid = roomRepository.findBySequenceAndUserId(sequence, userId).get().getUuid();
+        List<Voice> voices = voiceRepository.findByRoomUuid(roomUuid);
+        List<VoiceItem> voiceItems = new ArrayList<>();
+        for (Voice voice : voices) {
+            voiceItems.add(
+                    VoiceItem.builder()
+                            .voiceId(voice.getId())
+                            .registDate(voice.getRegistDate())
+                            .build()
+            );
+        }
+        return RadioVoicesResponse.builder()
+                .voiceItems(voiceItems)
+                .total(voices.size())
+                .build();
+    }
+    //음성 파일 리스트 조회 - 라디오 1
+
+    public RadioVoiceResponse getVoice(long voiceId) {
+        Voice voice = voiceRepository.findById(voiceId).get();
+        return RadioVoiceResponse.builder()
+                .voiceId(voiceId)
+                .voiceUrl(voice.getUrl())
+                .registDate(voice.getRegistDate())
+                .build();
+    }
+    //음성 파일 상세 조회 - 라디오 2
+
+    public void deleteVoice(long voiceId) {
+        voiceRepository.deleteById(voiceId);
+    }
+    //음성 파일 삭제 - 라디오 3
+}
