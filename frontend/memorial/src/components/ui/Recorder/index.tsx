3d3d8d067ed80c8562e82428bddca29c86fd3ec8
ninnistic <parkninni@icloud.com>
2023-09-26 00:43:31 +0900

    refactor: #39 - 수정된 audio library 타입에 따른 payphone, recorder 구문 타입 수정

--- frontend/memorial/src/components/ui/Recorder/index.tsx
+++ frontend/memorial/src/components/ui/Recorder/index.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
-import AudioReactRecorder, { RecordState } from "audio-react-recorder";
+import AudioReactRecorder, {
+  RecordState,
+  AudioData,
+} from "audio-react-recorder";
 
 import styles from "./Recorder.module.css";
 import { HiMicrophone } from "react-icons/hi2";
 import { HiStopCircle } from "react-icons/hi2";
 import { HiPauseCircle } from "react-icons/hi2";
-import { HiPlayCircle } from "react-icons/hi2";
 import AudioPlayer from "../AudioPlayer";
 
 interface RecorderProps {
-  onAudioDataReceived: (data: Blob | null) => void;
+  onAudioDataReceived: (data: AudioData | null) => void;
 }
 
 export default function Recorder({ onAudioDataReceived }: RecorderProps) {
   const [recordState, setRecordState] = useState(RecordState.STOP);
-  // const [audioData, setAudioData] = useState<Blob | null>(null);
-  const [audioData, setAudioData] = useState<any | null>(null);
+  const [audioData, setAudioData] = useState<AudioData | null>(null);
   const [recordingMessage, setRecordingMessage] = useState(""); // 녹음 상태 메시지
 
   // 녹음 시작
@@ -34,13 +35,13 @@ export default function Recorder({ onAudioDataReceived }: RecorderProps) {
     setRecordState(RecordState.PAUSE);
     setRecordingMessage("일시 중지됨");
   };
-  // 정지 후 재시작
-  const resumeRecording = () => {
-    setRecordState(RecordState.RESUME);
-    setRecordingMessage("녹음 중...");
-  };
+  // 정지 후 재시작 -> 존재하지 않는 prop이므로 comment처리함
+  // const resumeRecording = () => {
+  //   setRecordState(RecordState.RESUME);
+  //   setRecordingMessage("녹음 중...");
+  // };
 
-  const handleAudioData = (data: Blob) => {
+  const handleAudioData = (data: AudioData) => {
     setAudioData(data);
     onAudioDataReceived(data); // Blob 데이터를 전달합니다.
   };
@@ -67,11 +68,11 @@ export default function Recorder({ onAudioDataReceived }: RecorderProps) {
             <HiPauseCircle />
           </button>
         )}
-        {recordState === RecordState.PAUSE && (
+        {/* {recordState === RecordState.PAUSE && (
           <button className={styles.recordbtn} onClick={resumeRecording}>
             <HiPlayCircle />
           </button>
-        )}
+        )} */}
         <button className={styles.recordbtn} onClick={stopRecording}>
           <HiStopCircle />
         </button>
