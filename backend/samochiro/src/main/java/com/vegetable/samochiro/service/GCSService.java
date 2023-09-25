1caa03a6478c2bbf7b418a1dfdbf4b9d1806a99a
leunseongl <anytime0916@naver.com>
2023-09-25 10:02:47 +0900

    feat: #37 ai api 테스트 중

--- backend/samochiro/src/main/java/com/vegetable/samochiro/service/GCSService.java
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/service/GCSService.java
@@ -1,11 +1,20 @@
 package com.vegetable.samochiro.service;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.vegetable.samochiro.domain.Voice;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
@@ -25,8 +34,8 @@ public class GCSService {
         try{
             String rootPath = System.getProperty("user.dir");
             StorageOptions storageOptions = StorageOptions.newBuilder()
-                    .setProjectId(projectId)
-                    .setCredentials(GoogleCredentials.fromStream(new FileInputStream(rootPath + "/src/main/resources/avid-lock-397313-6129fb95bb28.json"))).build();
+                .setProjectId(projectId)
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(rootPath + "/src/main/resources/avid-lock-397313-6129fb95bb28.json"))).build();
             this.storage = storageOptions.getService();
         }
         catch (IOException e){
@@ -38,10 +47,10 @@ public class GCSService {
         try {
             String ext = file.getContentType(); // 파일의 형식 ex) JPG
             BlobInfo blobInfo = storage.create(
-                    BlobInfo.newBuilder(bucketName, fileName)
-                            .setContentType(ext)
-                            .build(),
-                    file.getInputStream()
+                BlobInfo.newBuilder(bucketName, fileName)
+                    .setContentType(ext)
+                    .build(),
+                file.getInputStream()
             );
             return blobInfo.getMediaLink();
         } catch (Exception e) {
@@ -55,4 +64,22 @@ public class GCSService {
         //버킷 명, 삭제 파일 이름, 프로젝트 ID
         storage.delete(bucketName, voicemailName, Storage.BlobSourceOption.decryptionKey(projectId));
     }
+
+    public List<Blob> downloadFiles(List<Voice> voiceList) {
+        List<Blob> blobList = new ArrayList<>();
+
+        for(Voice v: voiceList) {
+            String name = v.getName();
+            Blob blob = storage.get(bucketName, name);
+            blob.downloadTo(Paths.get(name.replaceAll(":","_")));
+            blobList.add(blob);
+
+        }
+        return blobList;
+    }
+
+    public Blob test(String name){
+        return storage.get(bucketName, name);
+    }
+
 }
