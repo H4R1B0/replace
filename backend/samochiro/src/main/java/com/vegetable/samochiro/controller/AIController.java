92cd294cc348276291a5a13595cf6c1d8d83607f
leunseongl <anytime0916@naver.com>
2023-09-26 17:16:52 +0900

    feat: ai response test

--- backend/samochiro/src/main/java/com/vegetable/samochiro/controller/AIController.java
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/controller/AIController.java
@@ -3,6 +3,7 @@ package com.vegetable.samochiro.controller;
 import com.vegetable.samochiro.dto.ai.AITrainingRequest;
 import com.vegetable.samochiro.service.AIService;
 import com.vegetable.samochiro.util.HeaderUtils;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +24,7 @@ public class AIController {
 	private final AIService aiService;
 	private final HeaderUtils headerUtils;
 
-	@PostMapping
+	@PostMapping("/training")
 	public void doTraining(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
 		@RequestBody AITrainingRequest request) {
 		System.out.println("request = " + request.getGender());
@@ -28,6 +32,19 @@ public class AIController {
 		aiService.doTraining(request, userId);
 	}
 	//학습하기 - ai 1번 (from front)
+
+	@PostMapping("/save")
+	public void getAIVoiceFile(@RequestPart String roomUuid,
+		@RequestPart List<MultipartFile> congratuationList,
+		@RequestPart List<MultipartFile> consolationList,
+		@RequestPart List<MultipartFile> encourageList ,
+		@RequestPart List<MultipartFile> safetyList,
+		@RequestPart List<MultipartFile> thanksList,
+		@RequestPart List<MultipartFile> welcomeList) {
+		System.out.println("옴");
+
+	}
+	//학습된 음성 데이터 받아서 저장 - from ai
 
 	//test용
 	@GetMapping
