bcdeff045ae38324384b35e7d41d66a1e33413e9
ninnistic <parkninni@icloud.com>
2023-09-27 01:34:58 +0900

    fix: audio, photo 업로드 되지 않던 문제 해결

--- frontend/memorial/src/components/ui/Modal/AudioRecordModal/index.tsx
+++ frontend/memorial/src/components/ui/Modal/AudioRecordModal/index.tsx
@@ -34,9 +34,9 @@ export default function AudioRecordModal({ ...other }: AudioRecordModalProps) {
   return (
     <Modal {...other} buttonLabel="close" onClose={() => navigate("..")}>
       <p>AudioRecordModal</p>
+      <Recorder onAudioDataReceived={(data) => setAudioData(data)} />
       <form onSubmit={handleFormSubmit}>
-        <Recorder onAudioDataReceived={(data) => setAudioData(data)} />
-        <Button>서버로 저장</Button>
+        <Button type="submit">서버로 저장</Button>
       </form>
     </Modal>
   );
