9a078aa35b06c981fd0b0e75dcb9e9a6ad6d8b77
H4R1B0 <qkrguswns25@naver.com>
2023-10-02 16:26:51 +0900

    test: 로그 확인 추가

--- backend/samochiro/src/main/java/com/vegetable/samochiro/oauth2/filter/JwtAuthenticationFilter.java
+++ backend/samochiro/src/main/java/com/vegetable/samochiro/oauth2/filter/JwtAuthenticationFilter.java
@@ -45,6 +45,8 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             httpResponse.getWriter().write(jsonErrorResponse);
             return; // 필터 체인을 종료합니다.
         }
+
+        System.out.println("토큰 통과");
         chain.doFilter(request, response);
     }
 
