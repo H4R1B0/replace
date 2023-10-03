715801b63c57c33ba5ae9986432f35d44ad443bd
Juahjoah <07juah21@gmail.com>
2023-10-04 02:21:58 +0900

    refactor: #45 - 로그아웃, 탈퇴 리팩토링 - api 분리 - 리액트쿼리

--- frontend/memorial/src/apis/user.ts
+++ frontend/memorial/src/apis/user.ts
@@ -1,9 +1,9 @@
 import { api, Header } from "./index";
 
 export const logout = () => {
-  return api.delete(`/user`, Header());
+  return api.delete(`/user`, {}, Header());
 };
 
 export const signout = () => {
-  return api.delete(`/user/me`, Header());
+  return api.delete(`/user/me`, {}, Header());
 };
