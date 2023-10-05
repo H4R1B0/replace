b1c12216fbe88b61ad0ee73f4a6dac7ea7f473a7
노수혁 <roh403@naver.com>
2023-10-05 16:09:14 +0900

    Fix: #48 roompage에 조건문 추가로 코드 원상복구

--- frontend/memorial/src/components/ui/Modal/DeleteRoomConfirmModal/index.tsx
+++ frontend/memorial/src/components/ui/Modal/DeleteRoomConfirmModal/index.tsx
@@ -1,11 +1,9 @@
-import { Fragment } from "react";
 import Button from "@components/ui/Button";
 import { useParams, useNavigate } from "react-router-dom";
 import Modal from "..";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { deleteSingleRoom } from "@apis/room";
 import toast from "react-hot-toast";
-import { useEffect, useState } from "react";
 
 import styles from "./DeleteRoomConfirmModal.module.css";
 
@@ -15,17 +13,7 @@ export default function DeleteRoomConfirmModal() {
   const roomSequence = parseInt(sequence);
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-
-  const { nickname } = useParams();
-  const username = nickname ?? "";
-
-  const [isVisitor, setIsVisitor] = useState(false);
-
-  useEffect(() => {
-    if (username !== sessionStorage.getItem("nickname")) {
-      setIsVisitor(true);
-    }
-  }, []);
+  const nickname = sessionStorage.getItem("nickname");
 
   const deleteSuccessToast = async () => {
     toast.success("방이 성공적으로 삭제되었습니다"), { id: "roomDeleted" };
@@ -47,25 +35,17 @@ export default function DeleteRoomConfirmModal() {
     deleteRoomMutation.mutate(roomSequence);
   };
 
-  const MyRoom = toast.error("자신의 방은 삭제할 수 없습니다.")
-  
-  const FriendRoom = toast.error("친구의 방은 삭제할 수 없습니다.")
-
-  const BuildInMyHouse = <Modal
-    title="정말 방을 지우시겠습니까?"
-    buttonLabel="닫기"
-    onClose={() => navigate("..")}
-  >
-    <div className={styles.buttonSection}>
-      <Button variant="delete" onClick={handleDelete}>
-        네, 정말 삭제할게요.
-      </Button>
-    </div>
-  </Modal>
-
   return (
-    <Fragment>
-      {isVisitor ? FriendRoom : (roomSequence === 1? MyRoom : BuildInMyHouse)}
-    </Fragment>
+    <Modal
+      title="정말 방을 지우시겠습니까?"
+      buttonLabel="닫기"
+      onClose={() => navigate("..")}
+    >
+      <div className={styles.buttonSection}>
+        <Button variant="delete" onClick={handleDelete}>
+          네, 정말 삭제할게요.
+        </Button>
+      </div>
+    </Modal>
   );
 }
